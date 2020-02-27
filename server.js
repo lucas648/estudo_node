428b11b54f1aaa2d575d6c7c3deb2eb4c8c15513
Lucas Costa <lucascosta_s_a@hotmail.com>
2020-02-27 16:21:10 -0300

    primeira versão

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+//iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodetest', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+requireDir('./src/models');
+
+//primeira rota 
+app.use('/', require('./src/routes'));
+
+app.listen(2020);
